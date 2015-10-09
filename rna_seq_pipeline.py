4bb0dbd23e7e2e0134b94b31dba9c9035741a7ce
Ubuntu <stuti.agrawal@gmail.com>
2015-10-09 19:32:24 +0000

    removed object store upload/download

--- rna_seq_pipeline.py
+++ rna_seq_pipeline.py
@@ -34,6 +34,10 @@ def run_pipeline(args, workdir, analysis_id, logger):
 
     pipelineUtil.log_function_time("STAR", analysis_id, star_cmd, logger)
 
+    remote_bam_path = "%s_star.bam" % os.path.join(args.bucket, analysis_id, analysis_id)
+    pipelineUtil.upload_to_cleversafe(logger, remote_bam_path, bam)
+
+
     cufflinks_cmd = ['time', '/usr/bin/time', 'python', args.cufflinks_pipeline,
                      '--bam', bam,
                      '--gtf', args.gtf,
@@ -45,8 +49,26 @@ def run_pipeline(args, workdir, analysis_id, logger):
 
     pipelineUtil.log_function_time("CUFFLINKS", analysis_id, cufflinks_cmd, logger)
 
+    cuffout_genes_local = os.path.join(star_output_dir, "genes.fpkm_tracking")
+    cuffout_genes_remote = os.path.join(args.bucket, "cufflinks", "star_gene", args.disease, "%s.genes.fpkm_tracking" %analysis_id)
+    pipelineUtil.upload_to_cleversafe(logger, cuffout_genes_remote, cuffout_genes_local)
+
+    cuffout_isoforms_local = os.path.join(star_output_dir, "isoforms.fpkm_tracking")
+    cuffout_isoforms_remote = os.path.join(args.bucket, "cufflinks", "star_iso", args.disease, "%s.isoforms.fpkm_tracking" %analysis_id)
+    pipelineUtil.upload_to_cleversafe(logger, cuffout_isoforms_remote, cuffout_isoforms_local)
+
     #pipelineUtil.remove_dir(star_output_dir)
 
+
+def download_missing_reference(args_input, remote_default, bucket):
+
+    path = os.path.dirname(args_input)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    pipelineUtil.download_from_cleversafe(None, os.path.join(bucket, remote_default), args_input)
+
+def download_from_alt_source(bucket, config, analysis_id, input_dir):
+    pipelineUtil.download_from_cleversafe(None, os.path.join(bucket, analysis_id), input_dir, config)
 
 if __name__ == "__main__":
 
@@ -58,18 +80,17 @@ if __name__ == "__main__":
     parser.add_argument('--disease', type=str, required=True, help='disease abbreviation')
 
     star = parser.add_argument_group("star pipeline")
-    star.add_argument('--genome_dir', default='/home/ubuntu/SCRATCH/star_genome_d1_vd1_gtfv22/', required=True,
-                     help='star index directory')
-    star.add_argument('--star_pipeline', default='/home/ubuntu/expression/icgc_rnaseq/star_align.py',
+    star.add_argument('--genome_dir', default='/home/ubuntu/SCRATCH/star_genome_d1_vd1_gtfv22/', help='star index directory')
+    star.add_argument('--star_pipeline', default='/home/ubuntu/icgc_rnaseq_align/star_align.py',
                       help='path to star pipeline')
-    star.add_argument('--input_dir', default='/home/ubuntu/SCRATCH', required=True, help='parent path for all datasets')
-    star.add_argument('--genome_fasta_file', type=str, help='path to reference genome', required=True
+    star.add_argument('--input_dir', default='/home/ubuntu/SCRATCH', help='parent path for all datasets')
+    star.add_argument('--genome_fasta_file', type=str, help='path to reference genome',
                 default='/home/ubuntu/SCRATCH/GRCh38.d1.vd1.fa')
     star.add_argument('--quantMode', type=str, default="", help='enable transcriptome mapping in STAR')
 
     cufflinks = parser.add_argument_group("cufflinks pipeline")
     cufflinks.add_argument('--cufflinks_pipeline', type=str,
-                            default='/home/ubuntu/expression/compute_expression.py')
+                            default='/home/ubuntu/lung_study/programs/compute_expression.py')
     args = parser.parse_args()
 
     analysis_id = args.analysis_id
@@ -77,19 +98,40 @@ if __name__ == "__main__":
     workdir = os.path.join(args.input_dir, analysis_id)
 
     if not os.path.isdir(workdir):
+        pipelineUtil.download_from_cleversafe(None, os.path.join(args.bucket, analysis_id), args.input_dir,
+                                              '/home/ubuntu/.s3cfg_cleversafe')
+
+    if not os.path.isdir(workdir):
+        download_from_alt_source('s3://tcga_cghub_protected', '/home/ubuntu/.s3cfg_cleversafe',
+                                analysis_id, args.input_dir)
+    if not os.path.isdir(workdir):
+        download_from_alt_source('s3://tcga_cghub_protected_2', '/home/ubuntu/.s3cfg_cleversafe',
+                                analysis_id, args.input_dir)
+    if not os.path.isdir(workdir):
+        download_from_alt_source('s3://tcga_cghub_protected_3', '/home/ubuntu/.s3cfg_cleversafe',
+                                analysis_id, args.input_dir)
+    if not os.path.isdir(workdir):
+        download_from_alt_source('s3://tcga_cghub_protected', '/home/ubuntu/.s3cfg_ceph',
+                                analysis_id, args.input_dir)
+
+    if not os.path.isdir(workdir):
         raise Exception("Cannot locate analysis_id %s" %analysis_id)
 
     if not os.path.isdir(args.genome_dir):
-        raise Exception("Cannot locate STAR genome build: %s" %args.genome_dir)
+        pipelineUtil.download_from_cleversafe(None, os.path.join(args.bucket, 'star_genome_d1_vd1_gtfv22'), args.input_dir)
 
     if not os.path.isfile(args.genome_fasta_file):
-        raise Exception("Cannot locate Genome FASTA File: %s" %args.genome_fasta_file)
+        default = "GRCh38.d1.vd1.fa"
+        download_missing_reference(args.genome_fasta_file, default, args.bucket)
 
     if not os.path.isfile(args.gtf):
-        raise Exception("Cannot locate GTF file: %s" %args.gtf)
+        default = "gencode.v22.annotation.gtf"
+        download_missing_reference(args.gtf, default, args.bucket)
 
     if os.path.isdir(workdir):
         star_log_file = "%s_star.log" %(os.path.join(args.input_dir, analysis_id, analysis_id))
         logger = setupLog.setup_logging(logging.INFO, analysis_id, star_log_file)
         run_pipeline(args, workdir, analysis_id, logger)
+        star_log_out = os.path.join(args.bucket, 'logs', args.disease, '%s.log' %analysis_id)
+        pipelineUtil.upload_to_cleversafe(logger,star_log_out, star_log_file)
         #pipelineUtil.remove_dir(workdir)
